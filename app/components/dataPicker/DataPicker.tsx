cef784e1723ef111e788a9b145fe2b6e39abda73
SashaKycherehko <sashakycherehko@gmail.com>
2023-08-01 23:52:13 -0700

    change data-picker

--- app/components/dataPicker/DataPicker.tsx
+++ app/components/dataPicker/DataPicker.tsx
@@ -1,135 +1,105 @@
 "use client";
+let date = new Date();
+let currentDay = date.getDay();
+let currentDate = date.getDate();
+let currentYear = date.getFullYear();
+let currentMonth = date.getMonth();
+const dayNames: string[] = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-import { debug } from "util";
+const monthNames: string[] = [
+  "Jauary",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-type FullYear = {
-  month: [
-    {
-      name_month: string;
-      max_day: number;
-      day: Array<number>;
-    }
-  ];
-  learYear: boolean;
-  year: number;
-};
-class DataPicker {
-  list_year: Array<FullYear> = [];
-
-  get getCurrentYear(): number {
-    let event: Date = new Date();
-    return event.getFullYear();
+function generateDaysInMonth(month: number, year: number) {
+  let dayInMonth = daysInMonth(month, year);
+  let arr: number[] = [];
+  for (let i = 1; i <= dayInMonth; i++) {
+    arr.push(i);
   }
 
-  public GenerateMonth(): void {
-    let count_month: number = 0;
-    let count_day: number = 0;
+  return arr;
+}
 
-    const dayNames: string[] = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    const monthNames: string[] = [
-      "Jauary",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-
-    console.log(
-      `${monthNames[0]}\t${monthNames[1]}\t${monthNames[2]}\t${monthNames[3]}\t${monthNames[4]}\t${monthNames[5]}\t${monthNames[6]}\t${monthNames[7]}\t${monthNames[8]}\t${monthNames[9]}\t${monthNames[10]}\t${monthNames[11]}`
-    );
-  }
-
-  generateListYear(list = this.list_year) {
-    return list.push({
-      month: [{ name_month: "Junary", max_day: 31, day: [23, 234] }],
-      learYear: false,
-      year: 2023,
-    });
-  }
-
-  private isLeapYear(month: number[]): boolean {
-    if (month.find((Item) => 29)) {
-      return true;
-    }
-
-    return false;
-  }
-  generateDay(count: number): number[] {
-    let arr: number[];
-    for (let i = 0; i < count; i++) {
-      arr.push(i);
-    }
-
-    return arr;
-  }
-
-  generateFullYear() {
-    let event: Date = new Date();
-    let current_day = event.getDay();
-    let current_year = event.getFullYear();
-    let current_month = event.getMonth();
-    let current_date = event.getDate();
-    let currentFullDayInYear = this.daysInYear(current_year);
-
-    console.log(current_day, current_month, current_date, current_year);
-
-    console.log(`${current_year}`, currentFullDayInYear);
-
-    for (let i: number = current_date; i < currentFullDayInYear; i++) {
-      debugger;
-      event.setDate(i);
-      let first_value = event.getDate();
-      let first_weekday = event.toLocaleDateString("en", { weekday: "long" });
-      console.log(`${first_value}\t:\t${first_weekday}`);
-      event.setMonth(current_month + 1);
-      console.log(event.getMonth());
-      event.setDate(0);
-      let second_value = event.getDate();
-      console.log(second_value);
-
-      for (let k: number = first_value; k < second_value; k++) {
-        if (k >= second_value) {
-          i = 0;
-        }
-      }
-      // let date = event.getDate();
-      // let second_weekday = event.toLocaleDateString("en", { weekday: "long" });
-    }
-  }
-
-  daysInMonth(month: number, year: number): number {
-    return new Date(year, month, 0).getDate();
-  }
-
-  daysInYear(year: number) {
-    var days = 0;
-
-    for (var month = 1; month <= 12; month++) {
-      days += this.daysInMonth(month, year);
-    }
-
-    return days;
-  }
+function daysInMonth(month: number, year: number): number {
+  return new Date(year, month, 0).getDate();
 }
 
 export const DatePicker = (): React.ReactNode => {
-  let data_picker: DataPicker = new DataPicker();
-  data_picker.generateFullYear();
-  return <>{}</>;
+  let arr: number[] = generateDaysInMonth(currentMonth, currentYear);
+  let objdate = new Date();
+  let today = new Date(
+    currentYear,
+    currentMonth,
+    currentDate
+  ).toLocaleDateString("default", { weekday: "short" });
+  let tomorrow = new Date(
+    currentYear,
+    currentMonth,
+    currentDate + 1
+  ).toLocaleDateString("default", { weekday: "short" });
+  // let thisWeekend = new Date(
+  //   currentYear,
+  //   currentMonth,
+  //   currentDate + 7
+  // ).toLocaleDateString("default", { weekday: "short", day: "numeric" });
+  let nextWeek = new Date(
+    currentYear,
+    currentMonth,
+    currentDate + 7
+  ).toLocaleDateString("default", { weekday: "short", day: "numeric" });
+
+  return (
+    <>
+      <div className="data-picker">
+        <div className="data-today">{today}</div>
+        <div className="data-tomorrow">{tomorrow}</div>
+        <div className="data-nextWeek">{nextWeek}</div>
+        <div className="data-generates">
+          <div className="data-title">
+            <div className="data-month"></div>
+            <div className="data-day"></div>
+
+            <div className="data-buttons">
+              <div className="button-back">
+                <button>Back</button>
+              </div>
+              <button className="button-restore">restore</button>
+              <div className="button-next">
+                <button>Next</button>
+              </div>
+            </div>
+          </div>
+          <div className="data-nameOfWeek">
+            <h1>Sunday</h1>
+            <h1>Monday</h1>
+            <h1>Tuesday</h1>
+            <h1>Wednesday</h1>
+            <h1>Thursday</h1>
+            <h1>Friday</h1>
+            <h1>Saturday</h1>
+          </div>
+          <div className="data-days"></div>
+        </div>
+      </div>
+    </>
+  );
 };
