a916a22cdf61d8db48a49998c8338f431bb15e7a
SashaKycherehko <sashakycherehko@gmail.com>
2023-08-19 04:29:26 -0700

    modal-date modal-priority

--- components/modals-window/add-task/add_task.tsx
+++ components/modals-window/add-task/add_task.tsx
@@ -94,7 +94,9 @@ export const AddTask = () => {
                         </svg>
                         <span>Tomorrow</span>
                       </div>
-                      <div className="modal-task-date">Wen</div>
+                      <div className="modal-task-date">
+                        <span>Wen</span>
+                      </div>
                     </button>
                     <button className="action-date modal-task-editor_nextweek">
                       <div className="modal-task-date_name">
@@ -185,8 +187,140 @@ export const AddTask = () => {
                         </button>
                       </div>
                     </div>
-                    <div className="data-picker-weekDaysName"></div>
-                    <div className="data-picker-monthlist"></div>
+                    <div className="data-picker-weekDaysName">
+                      <span>S</span>
+                      <span>M</span>
+                      <span>T</span>
+                      <span>W</span>
+                      <span>T</span>
+                      <span>F</span>
+                      <span>S</span>
+                    </div>
+                    <div className="data-picker-monthlist">
+                      <div className="data-picker-month">
+                        <div className="data-picker-month_name">
+                          <span>Aug</span>
+                        </div>
+                        <div className="data-picker-month_days">
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                        </div>
+                      </div>
+                      <div className="data-picker-month">
+                        <div className="data-picker-month_name">
+                          <span>Aug</span>
+                        </div>
+                        <div className="data-picker-month_days">
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                        </div>
+                      </div>
+                      <div className="data-picker-month">
+                        <div className="data-picker-month_name">
+                          <span>Aug</span>
+                        </div>
+                        <div className="data-picker-month_days">
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                          <span>1</span>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="data-picker-time">
+                      <button className="data-picker-time_button">
+                        + Add time
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -223,6 +357,84 @@ export const AddTask = () => {
                       fill="currentColor"
                     ></path>
                   </svg>
+                </div>
+                <div className="modal-action modal-task-priority">
+                  <div className="modal-task-priority_select">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      data-icon-name="priority-icon"
+                      data-priority="4"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 3a.5.5 0 01.276-.447C3.025 2.179 4.096 2 5.5 2c.901 0 1.485.135 2.658.526C9.235 2.885 9.735 3 10.5 3c1.263 0 2.192-.155 2.776-.447A.5.5 0 0114 3v6.5a.5.5 0 01-.276.447c-.749.375-1.82.553-3.224.553-.901 0-1.485-.135-2.658-.526C6.765 9.615 6.265 9.5 5.5 9.5c-1.08 0-1.915.113-2.5.329V13.5a.5.5 0 01-1 0V3zm1 5.779v-5.45C3.585 3.113 4.42 3 5.5 3c.765 0 1.265.115 2.342.474C9.015 3.865 9.599 4 10.5 4c1.002 0 1.834-.09 2.5-.279v5.45c-.585.216-1.42.329-2.5.329-.765 0-1.265-.115-2.342-.474C6.985 8.635 6.401 8.5 5.5 8.5c-1.001 0-1.834.09-2.5.279z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    <span>Prority 1</span>
+                  </div>
+                  <div className="modal-task-priority_select">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      data-icon-name="priority-icon"
+                      data-priority="4"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 3a.5.5 0 01.276-.447C3.025 2.179 4.096 2 5.5 2c.901 0 1.485.135 2.658.526C9.235 2.885 9.735 3 10.5 3c1.263 0 2.192-.155 2.776-.447A.5.5 0 0114 3v6.5a.5.5 0 01-.276.447c-.749.375-1.82.553-3.224.553-.901 0-1.485-.135-2.658-.526C6.765 9.615 6.265 9.5 5.5 9.5c-1.08 0-1.915.113-2.5.329V13.5a.5.5 0 01-1 0V3zm1 5.779v-5.45C3.585 3.113 4.42 3 5.5 3c.765 0 1.265.115 2.342.474C9.015 3.865 9.599 4 10.5 4c1.002 0 1.834-.09 2.5-.279v5.45c-.585.216-1.42.329-2.5.329-.765 0-1.265-.115-2.342-.474C6.985 8.635 6.401 8.5 5.5 8.5c-1.001 0-1.834.09-2.5.279z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    <span>Prority 2</span>
+                  </div>
+                  <div className="modal-task-priority_select">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      data-icon-name="priority-icon"
+                      data-priority="4"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 3a.5.5 0 01.276-.447C3.025 2.179 4.096 2 5.5 2c.901 0 1.485.135 2.658.526C9.235 2.885 9.735 3 10.5 3c1.263 0 2.192-.155 2.776-.447A.5.5 0 0114 3v6.5a.5.5 0 01-.276.447c-.749.375-1.82.553-3.224.553-.901 0-1.485-.135-2.658-.526C6.765 9.615 6.265 9.5 5.5 9.5c-1.08 0-1.915.113-2.5.329V13.5a.5.5 0 01-1 0V3zm1 5.779v-5.45C3.585 3.113 4.42 3 5.5 3c.765 0 1.265.115 2.342.474C9.015 3.865 9.599 4 10.5 4c1.002 0 1.834-.09 2.5-.279v5.45c-.585.216-1.42.329-2.5.329-.765 0-1.265-.115-2.342-.474C6.985 8.635 6.401 8.5 5.5 8.5c-1.001 0-1.834.09-2.5.279z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    <span>Prority 3</span>
+                  </div>
+                  <div className="modal-task-priority_select">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      data-icon-name="priority-icon"
+                      data-priority="4"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 3a.5.5 0 01.276-.447C3.025 2.179 4.096 2 5.5 2c.901 0 1.485.135 2.658.526C9.235 2.885 9.735 3 10.5 3c1.263 0 2.192-.155 2.776-.447A.5.5 0 0114 3v6.5a.5.5 0 01-.276.447c-.749.375-1.82.553-3.224.553-.901 0-1.485-.135-2.658-.526C6.765 9.615 6.265 9.5 5.5 9.5c-1.08 0-1.915.113-2.5.329V13.5a.5.5 0 01-1 0V3zm1 5.779v-5.45C3.585 3.113 4.42 3 5.5 3c.765 0 1.265.115 2.342.474C9.015 3.865 9.599 4 10.5 4c1.002 0 1.834-.09 2.5-.279v5.45c-.585.216-1.42.329-2.5.329-.765 0-1.265-.115-2.342-.474C6.985 8.635 6.401 8.5 5.5 8.5c-1.001 0-1.834.09-2.5.279z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    <span>Prority 4</span>
+                  </div>
                 </div>
               </div>
               <div className="modal-task-editor_reminders">
