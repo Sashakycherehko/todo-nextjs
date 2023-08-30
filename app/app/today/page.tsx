b1d674d56d7b5a47ea1b8ef6963303e30933cd29
SashaKycherehko <sashakycherehko@gmail.com>
2023-08-30 09:27:17 -0700

    test

--- app/app/today/page.tsx
+++ app/app/today/page.tsx
@@ -3,8 +3,10 @@ import { MainContainer } from "@/components/main-container/main_container";
 import { MainSection } from "@/components/main-section/main_section";
 import { AddTask } from "@/components/modals-window/add-task/add_task";
 import { Navigation } from "@/components/navigation/navigation";
+import { useState } from "react";
 
 export const Today = (): React.ReactNode => {
+  const [isOpenMenu, setOpenMenu] = useState<boolean>();
   return (
     <>
       <Navigation>{<></>}</Navigation>
